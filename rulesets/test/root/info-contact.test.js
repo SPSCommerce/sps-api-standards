d4a9b1f525df29ba781c57f0ffbb25695acbce57
Travis Gosselin <travisgosselin@users.noreply.github.com>
2022-06-03 11:19:53 -0400

    fix: Spectral Tests Added and Ruleset Terminology updated from "MUST" to "SHOULD" (#18)

---
+++ rulesets/test/root/info-contact.test.js
@@ -0,0 +1,27 @@
+const { SpectralTestHarness } = require("../harness/spectral-test-harness.js");
+
+describe("info-contact", () => {
+    let spectral = null;
+    const ruleName = expect.getState().currentTestName;
+    const ruleset = "src/root.ruleset.yml";
+
+    beforeEach(async () => {
+        spectral = new SpectralTestHarness(ruleset);
+    });
+
+    test("info-contact-is-disabled", async () => {
+        const spec = `
+            openapi: 3.1.0
+            info:
+                title: My API
+                description: my-thing
+                version: 1.0.0
+            servers:
+                - description: server1
+                  url: http://localhost:8080
+            paths: {}
+        `;
+    
+        await spectral.validateSuccess(spec, ruleName);
+    });
+});
