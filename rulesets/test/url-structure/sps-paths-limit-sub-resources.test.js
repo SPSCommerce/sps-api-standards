db195e78ae7a63f680c80b2a0edba8468fbc355e
Travis Gosselin <travisgosselin@users.noreply.github.com>
2023-06-21 16:46:46 -0400

    feat: Localhost Exceptions & Inaccurate Long Resource Paths (#57)
    * Updates for localhost usage / testing / design
    * Fixes #31

--- rulesets/test/url-structure/sps-paths-limit-sub-resources.test.js
+++ rulesets/test/url-structure/sps-paths-limit-sub-resources.test.js
@@ -21,11 +21,24 @@ describe("sps-paths-limit-sub-resources", () => {
         await spectral.validateSuccess(spec, ruleName);
     });
 
-    test("fails after 3 hierarchy levels", async () => {
+    test("succeeds with a lot of resources", async () => {
         const spec = `
             openapi: 3.1.0
             paths:
-                /v1/users/{userId}/profiles/{profileId}/images/{imageId}/another-level:
+                /v1/this/resource/should/not/fail/queue/purge:
+                    get:
+                        summary: hello
+        `;
+       
+        await spectral.validateSuccess(spec, ruleName);
+    });
+
+    
+    test("fails after too many sub resources", async () => {
+        const spec = `
+            openapi: 3.1.0
+            paths:
+                /v1/this/resource/should/not/fail/queue/purge/failed:
                     get:
                         summary: hello
         `;
