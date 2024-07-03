1fa9a1b1e64dc5416177791078c6baba2cb5c08f
Ethan Honzik <ehonzik@spscommerce.com>
2024-07-03 15:06:07 -0500

    updated sps-camel-case-properties to use built in spectral functionality, added sps-disallowed-prepositions rule

---
+++ rulesets/test/naming/sps-disallowed-prepositions.test.js
@@ -0,0 +1,132 @@
+const { SpectralTestHarness } = require("../harness/spectral-test-harness.js");
+
+describe("sps-disallowed-prepositions", () => {
+  let spectral = null;
+  const ruleName = "sps-disallowed-prepositions";
+  const ruleset = "src/naming.ruleset.yml";
+
+  beforeEach(async () => {
+    spectral = new SpectralTestHarness(ruleset);
+  });
+
+  test("valid property name", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        name:
+                          type: string
+            `;
+
+    await spectral.validateSuccess(spec, ruleName);
+  });
+
+  test("valid property name", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        testFor:
+                          type: string
+            `;
+    await spectral.validateFailure(spec, ruleName, "Warning", 1);
+  });
+
+
+  test("invalid property name for", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        for:
+                          type: string
+        `;
+
+    await spectral.validateFailure(spec, ruleName, "Warning", 1);
+  });
+
+  test("invalid property name forSomething", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        forSomething:
+                          type: string
+        `;
+
+    await spectral.validateFailure(spec, ruleName, "Warning", 1);
+  });
+
+  test("invalid property name at", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        at:
+                          type: string
+        `;
+
+    await spectral.validateFailure(spec, ruleName, "Warning", 1);
+  });
+  
+});
