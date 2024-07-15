071fd460a6368fe9004606c2cf52b5bbbc32366d
Ethan Honzik <ehonzik@spscommerce.com>
2024-07-15 12:53:45 -0500

    updated test for boolean prefix

--- rulesets/test/naming/sps-disallowed-boolean-prefixes.test.js
+++ rulesets/test/naming/sps-disallowed-boolean-prefixes.test.js
@@ -9,6 +9,46 @@ describe("sps-disallowed-boolean-prefixes", () => {
     spectral = new SpectralTestHarness(ruleset);
   });
 
+  test("all invalid property names", async () => {
+    const spec = `
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        version: 1.0.0
+      paths:
+        /users:
+          get:
+            responses:
+              '200':
+                description: A list of users
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        is:
+                          type: boolean
+                        has:
+                          type: boolean
+                        was:
+                          type: boolean
+                        will:
+                          type: boolean
+                        needs:
+                          type: boolean
+                        uses:
+                          type: boolean
+                        should:
+                          type: boolean
+                        can:
+                          type: boolean
+                        validPropertyName:
+                          type: boolean
+            `;
+
+    await spectral.validateFailure(spec, ruleName, "Warning", 8);
+  });
+
   test("invalid property name isTest", async () => {
     const spec = `
       openapi: 3.0.0
@@ -105,7 +145,7 @@ describe("sps-disallowed-boolean-prefixes", () => {
     await spectral.validateSuccess(spec, ruleName);
   });
 
-  test("invalid property name at", async () => {
+  test("valid property name at", async () => {
     const spec = `
       openapi: 3.0.0
       info:
